24f2951699a4c6b599c979a5ec0783ac1e5141fa
AdrianValentinCiu <111737211+AdrianValentinCiu@users.noreply.github.com>
2023-03-24 12:14:24 +0200

    show signature form .csv file

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -3,7 +3,12 @@
 
 #include "stdafx.h"
 #include "common.h"
+#include <fstream>
 
+struct DataCSV {
+	double x;
+	double y;
+};
 
 void testOpenImage()
 {
@@ -64,9 +69,82 @@ void testColor2Gray()
 	}
 }
 
+std::vector<DataCSV> readCSV()
+{
+	std::ifstream file("test_points.csv");
+	std::vector<DataCSV> data;
+	// Check if file is opened successfully
+	if (!file.is_open()) {
+		std::cerr << "Error opening file" << std::endl;
+		return data;
+	}
+
+	std::string line, field;
+
+	// Read each line of the file
+	while (std::getline(file, line)) {
+		std::stringstream ss(line);
+
+		// Parse the x and y values from each line
+		DataCSV row;
+		std::getline(ss, field, ',');
+		row.x = std::stod(field);
+		std::getline(ss, field, ',');
+		row.y = std::stod(field);
+
+		data.push_back(row);
+	}
+
+	// Print the data to the console
+	for (const auto& row : data) {
+		std::cout << "x: " << row.x << ", y: " << row.y << std::endl;
+	}
+
+	file.close(); // Close the file
+	return data;
+}
+
+void drawPoints(const std::vector<DataCSV>& data, Mat& image)
+{
+	std::vector<cv::Point2f> points;
+	for (const auto& row : data) {
+		points.emplace_back(row.x, row.y);
+	}
+	for (const auto& point : points)
+	{
+		circle(image, point, 5, Scalar(255, 255, 255), FILLED);
+	}
+	std::cout << "done draw data" << "\n";
+}
+
+void showDataFromCSV()
+{
+	
+	std::vector<DataCSV> points = readCSV();
+
+	// Create an image to draw the points on
+	Mat_<uchar> img = Mat::zeros(Size(1500, 720), CV_8UC1);
+
+	// Draw the points on the image
+	drawPoints(points, img);
+
+	// Show the image
+	imshow("Points_from_Signature", img);
+	waitKey(0);
+	
+}
+
+void testShowDataFromCSV()
+{
+	showDataFromCSV();
+}
+
 
 int main()
 {
+	//readCSV();
+	showDataFromCSV();
+	/*
 	int op;
 	do
 	{
@@ -76,9 +154,10 @@ int main()
 		printf(" 1 - Basic image opening...\n");
 		printf(" 2 - Open BMP images from folder\n");
 		printf(" 3 - Color to Gray\n");
+		printf(" 4 - Show data from csv file\n");
 		printf(" 0 - Exit\n\n");
 		printf("Option: ");
-		scanf("%d",&op);
+		scanf("%d", &op);
 		switch (op)
 		{
 			case 1:
@@ -90,8 +169,12 @@ int main()
 			case 3:
 				testColor2Gray();
 				break;
+			case 4:
+				testShowDataFromCSV();
+				break;
 		}
 	}
 	while (op!=0);
+	*/
 	return 0;
 }
